722250996dcbf15ac2691325278abf645f011ec4
Peter McAree <me@petermcaree.com>
2021-01-25 23:48:29 +0000

    Feat/add tree endpoints (#1)
    * Added missing comma
    
    * Added env tree endpoints
    
    Co-authored-by: ChloeMcAteer <chloemcateer97@gmail.com>

--- app.py
+++ app.py
@@ -69,3 +69,52 @@ def get_crime_data_by_year(table_name, year):
       parsed_items = replace_decimals(response["Items"])
 
       return jsonify(parsed_items)
+
+
+@app.route("/environment/<table_name>", methods=['GET'])
+def get_env_data(table_name):
+    """
+    Environment data
+    ---
+    get:
+      description: Get environment data about Belfast trees
+      responses:
+        200
+    """
+    limit = 10 if request.args.get('limit') is None else int(request.args.get('limit'))
+    table = dynamo_client.Table(table_name)
+    response = table.scan(Limit=limit)
+
+    # Parses the boto3 Decimal objects into native Py integers
+    parsed_items = replace_decimals(response["Items"])
+
+    return jsonify(parsed_items)
+
+
+@app.route("/environment/<table_name>/tree/<species>", methods=['GET'])
+def get_env_data_by_tree(table_name, species):
+    """
+    Environment data
+    ---
+    get:
+      description: Get environment data about Belfast trees by tree type
+      responses:
+        200
+    """
+    table = dynamo_client.Table(table_name)
+
+    if species is None:
+      return Response(status=400)
+    else:
+      response = table.scan(
+          FilterExpression="SPECIESTYPE = :SPECIESTYPE",
+          ExpressionAttributeValues={
+          ":SPECIESTYPE": species
+          }
+      )
+      
+      # Parses the boto3 Decimal objects into native Py integers
+      parsed_items = replace_decimals(response["Items"])
+
+      return jsonify(parsed_items)
+
