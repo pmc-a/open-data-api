8603383beed2d1ee1e41ff69822bc2c304eb992c
Peter McAree <me@petermcaree.com>
2021-01-25 23:01:37 +0000

    add: query ability to endpoint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, Response, jsonify, request
+from boto3.dynamodb.conditions import Key
 import boto3
 
 from src.utils import replace_decimals
@@ -34,7 +35,17 @@ def get_crime_data(table_name):
     limit = 10 if request.args.get('limit') is None else int(request.args.get('limit'))
     table = dynamo_client.Table(table_name)
 
-    response = table.scan(Limit=limit)
+    filter_value_present = False if request.args.get('year') is None else True
+
+    if filter_value_present:
+      response = table.scan(
+          FilterExpression="Calendar_Year = :Calendar_Year",
+          ExpressionAttributeValues={
+          ":Calendar_Year": int(request.args.get('year'))
+          }
+      )
+    else:
+      response = table.scan(Limit=limit)
 
     # Parses the boto3 Decimal objects into native Py integers
     parsed_items = replace_decimals(response["Items"])
