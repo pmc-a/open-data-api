45f59e42facfa5806bb3acfa5ab6192384bc3c16
Peter McAree <me@petermcaree.com>
2021-01-25 21:27:38 +0000

    add: endpoint docs

--- app.py
+++ app.py
@@ -3,18 +3,34 @@ import boto3
 
 from src.utils import replace_decimals
 
-
 app = Flask(__name__)
 dynamo_client = boto3.resource('dynamodb')
 
 
 @app.route('/health', methods=['GET'])
 def health_check():
+    """
+    Health Check
+    ---
+    get:
+      description: Health check to verify service is running
+      responses:
+        200:
+            description: The resource was deleted successfully.
+    """
     return Response(status=200)
 
 
 @app.route("/crime/<table_name>", methods=['GET'])
 def get_crime_data(table_name):
+    """
+    Crime data
+    ---
+    get:
+      description: Get crime data
+      responses:
+        200
+    """
     table = dynamo_client.Table(table_name)
 
     response = table.scan()
